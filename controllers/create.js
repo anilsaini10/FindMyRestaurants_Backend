5921c52455123b13dae2e9fd6ea2c88100900a48
Suraj Yadav <syadav32940@gmail.com>
2022-11-04 00:23:20 +0530

    manger name and password

--- controllers/create.js
+++ controllers/create.js
@@ -1,15 +1,12 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const url = require("../URL/url");
-
-
-const createRestaurent = async (req, res) => {
-
-    console.log(req.body);
-    const uri = url;
+const uri = url;
     const client = new MongoClient(uri);
 
     const db = client.db("Restaurent");
+const createRestaurent = async (req, res) => {
 
+    console.log(req.body);
     const collection = db.collection("RestaurentList");
 
     await collection.insertOne({
@@ -21,11 +18,19 @@ const createRestaurent = async (req, res) => {
 
     return res.send({ "Success": "done" });
 };
+const createOwner= async (req,res)=>{
+    const resturentOwner=db.collection("resturentOwner");
+
+    await resturentOwner.insertOne({
+        "ownerName":req.body.name,
+        "contact":req.body.number,
+    });
+    return res.send({"Success":"Done"});
+};
 
 
 module.exports = {
 
-    createRestaurent: createRestaurent
-
-
+    createRestaurent: createRestaurent,
+    createOwner:createOwner
 };
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const createPath = require("../controllers/create.js");
 const read_path = require("../controllers/read.js");
 
 route.post("/", createPath.createRestaurent);
+route.post("/suraj",createPath.createOwner);
 // route.get("/purchase_and_shipment_details", read_path.purchase_and_shipment_details);
 
 
